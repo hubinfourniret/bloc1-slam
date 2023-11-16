875f61de32226ecaa55766bc7332f5439a012e8f
hubinfourniret <hubin.fourniret@sts-sio-caen.info>
2023-11-16 17:03:47 +0100

    123

---
+++ TD/TD5/Lorem-ipsum-gen.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport"
+         content="width=device-width, user-scalable=no, initial-scale=1,0, maximum-scale=1.0, minimum-scale=1.0">
+   <meta http-equiv="X-UA-compatible" content="ie=edge">
+   <title>message</title>
+</head>
+<?php
+$size = $size ??;
+$_FILES["paragraphe.yaml"]
+echo : $_FILES
+?>
+<form action="<?=$_SERVER["PHP_SELF"]?>">
+<body>
+    <div>
+        <label for="para">Nombre de paragraphe :</label><input type="number" id="para" name="para" value="<?=$para?>"min="1" max="30">
+    </div>
+    <button type="submit">Valider</button>
+</body>
+</form>
